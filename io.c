9b9dfa7d88f9042f0799b1889ce38191d14fb1ba
aaron-jencks <aaronjencks@aol.com>
2025-06-10 18:30:43 -0400

    abstracted io

---
+++ io.c
@@ -0,0 +1,33 @@
+#include "io.h"
+
+#include <stddef.h>
+#include <stdio.h>
+
+
+void copy_into_center(uint8_t** dest, coord_t dsize,
+                      uint8_t** src, coord_t ssize) {
+    size_t row_offset = (dsize.y > ssize.y) ? (dsize.y - ssize.y) / 2 : 0;
+    size_t col_offset = (dsize.x > ssize.x) ? (dsize.x - ssize.x) / 2 : 0;
+
+    for (size_t i = 0; i < ssize.y && (row_offset + i) < dsize.y; i++) {
+        for (size_t j = 0; j < ssize.x && (col_offset + j) < dsize.x; j++) {
+            dest[row_offset + i][col_offset + j] = src[i][j];
+        }
+    }
+}
+
+void draw_grid(grid_t current, grid_t previous, coord_t term) {
+    coord_t cc;
+    for (size_t i = 0; i < current.size.y && i < term.y; i++) {
+        for (size_t j = 0; j < current.size.x && j < term.x; j++) {
+            if (current.grid[i][j] != previous.grid[i][j]) {
+                cc.x = j;
+                cc.y = i;
+                cursor_move(cc);
+                putchar(current.grid[i][j] ? '*' : ' ');
+                previous.grid[i][j] = current.grid[i][j];
+            }
+        }
+    }
+    fflush(stdout);
+}
---
+++ io.h
@@ -0,0 +1,12 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdint.h>
+
+#include "grid.h"
+#include "types.h"
+
+void copy_into_center(uint8_t** dest, coord_t dsize, uint8_t** src, coord_t ssize);
+void draw_grid(grid_t current, grid_t previous, coord_t term);
+
+#endif
